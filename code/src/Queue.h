a1bac8f99f6cba96ad7da6876e8b6158acfe9f10
dime1897 <diego.mercoliano@live.com>
2023-09-08 12:19:39 +0200

    Implementata la definitiva versione della sincornizzazione con un solo metodo pop ed un solo metodo push. Modificato main Testing.cpp

--- code/src/Queue.h
+++ code/src/Queue.h
@@ -12,14 +12,20 @@ using namespace std;
 #ifndef FIXED_PRIORITY
 #define FIXED_PRIORITY 1
 #endif
+#ifndef RELIABILITY
+#define RELIABILITY 1
+#endif
 #ifndef DYNAMIC_PRIORITY
 #define DYNAMIC_PRIORITY 2
+#endif
+#ifndef BEST_EFFORT
+#define BEST_EFFORT 0
 #endif
 #ifndef DIM
 #define DIM 100
 #endif
 #ifndef THREADS
-#define THREADS 2000
+#define THREADS 1000
 #endif
 
 template<class T>
@@ -43,31 +49,33 @@ class Queue{
      * Queue<int> q((size_t)10) --> SI
      * **/
 
-        Queue(int type = FIFO, int levels = 1, size_t dim = DIM);
+        Queue(int gest = BEST_EFFORT, int type = FIFO, int levels = 1, size_t dim = DIM);
 
-        Queue(size_t dim);
+        Queue(size_t dim, int gest = BEST_EFFORT);
 
         void setLevels(int);
 
         int getLevels(); //Ritorna il numero di livelli della coda (che coincide con la priorità massima)
+        int getType(); //Ritorna il tipo di coda
 
-        T popBestEffort(int priority = -1);
-        T popReliability(int priority = -1);/*
+        T pop();/*
         Estrae il primo elemento della coda NON VUOTA di più alta priorità.
         */
-
-        void pushBestEffort(T element, int priority = -1);
-        void pushReliability(T element, int priority = -1);/*
+        void push(T element, int priority = -1);/*
         Inserisce l'elemento passato nella coda di priorità specificata (se a code multiple). Per utilizzare la push sulla coda FIFO, NON SPECIFICARE la priorità, oppure usare 0.
         */
 
     private:
+        void show();
+        bool generalEmpty(); //Ritorna TRUE se tutti livelli di priorità sono completamente vuoti; FALSE altrimenti.
+
         bool* empty;
         bool* full;/*
         EMPTY e FULL sono due array lunghi quanto il numero di livelli di priorità presenti. Ogni posizione indica se la coda del corrispondente livello di priorità è piena o vuota.
         */
 
         int type;
+        int gest;
         int levels;
         T **queue;
         int dim;
@@ -79,10 +87,12 @@ class Queue{
         ma per coerenza alla letteratura si mantengono anche quelli.
         */
 
-        int* push_block; //È un array perché indicizza il prossimo che dovrà essere svegliato, ma si ha un indice per ogni livello di priorità.
-        int* pop_block; //Per tenere traccia di quanti si sono bloccati in lettura (per come è implementata la sincronizzazione, permette il risveglio selettivo in ordine FIFO).
+        int* push_block; //Array che contiene in ogni posizione l'indice del semaforo su cui si dovrà bloccare il thread.
+        int* push_wakeup; //Array che contiene in ogni posizione l'indice del prossimo thread da svegliare.
+        int pop_block; //Indice del semaforo si cui si dovrà bloccare il thread.
+        int pop_wakeup; //Indice del prossimo thread da svegliare.
         sem_t mutex; //Per accedere in maniera mutuamente esclusiva alla coda.
-        sem_t** sem_empty; //Semaforo su cui si blocca chi vuole leggere ma la coda è vuota.
+        sem_t* sem_empty; //Semaforo su cui si blocca chi vuole leggere ma la coda è vuota.
         sem_t** sem_full; /*
         È una matrice perché i pushers si bloccano selettivamente su uno specifico livello di priorità. È questa matrice è l'unica soluzione che mi è venuta in mente per un risveglio
         selettivo sia in termini di livello di priorità che per mantenere l'ordine FIFO.
